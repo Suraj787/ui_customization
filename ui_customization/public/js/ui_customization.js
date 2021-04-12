bebccd2f49866b7f1b579e572c4ea6f8e07645c6
biswa4u85 <biswa4u85@gmail.com>
2021-04-12 15:36:37 +0000

    upsupd logo

--- ui_customization/public/js/ui_customization.js
+++ ui_customization/public/js/ui_customization.js
@@ -1,8 +1,21 @@
-frappe.model.get_value('Print Settings', {'name': 'Print Settings'}, 'pdf_page_size',
-  function(d) {
-    console.log(d)
-  })
+// (function() {
+//   var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+//   link.type = 'image/x-icon';
+//   link.rel = 'icon';
+//   link.href = '/assets/images/fav.png';
+//   document.getElementsByTagName('head')[0].appendChild(link);
+// })();
+document.querySelector("link[rel*='icon']").href = "/assets/images/fav.png";
+
+// frappe.model.get_value('Print Settings', {'name': 'Print Settings'}, 'pdf_page_size',
+//   function(d) {
+//     console.log(d)
+// })
   
+// frappe.model.get_value('Site Settings', {'name': 'Site Settings'}, 'logo',
+//   function(d) {
+//     console.log(d)
+// })
   
 //   frappe.ui.form.on('Site Settings', {
 //     // frm passed as the first parameter
@@ -11,3 +24,6 @@ frappe.model.get_value('Print Settings', {'name': 'Print Settings'}, 'pdf_page_s
 //         // write setup code
 //     }
 // })
+
+
+	
